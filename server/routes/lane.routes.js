df57420589a708e52d4cde8e6b8ff8631116daa4
ASP <dragon.ka@wp.pl>
2018-02-18 20:26:17 +0100

    Problem

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -2,6 +2,12 @@ import { Router } from 'express';
 import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
-
+//Get all Lanes
+router.route('/lanes').get(LaneController.getLanes);
+//Add new Line
+router.route('/lanes').post(LaneController.addLane);
+//Delete a lane by LaneId
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
 
 export default router;
+
